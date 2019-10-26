2abf79c31bdeb253c64eca7d406886acb67a5df2
Anirup Patnaik <anirup049@gmail.com>
2019-10-26 17:11:15 +0530

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Anirup Patnaik on 26/10/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
