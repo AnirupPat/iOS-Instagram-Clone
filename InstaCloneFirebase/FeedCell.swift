ca4414ca92953b9ec7ba4d639c190e371d434c5a
Anirup Patnaik <anirup049@gmail.com>
2019-11-02 15:52:24 +0530

    first commit

---
+++ InstaCloneFirebase/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstaCloneFirebase
+//
+//  Created by Anirup Patnaik on 30/10/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
